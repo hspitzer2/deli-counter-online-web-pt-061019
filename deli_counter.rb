97d4f8a86bd0d3f91fe06d5868699c3f40ac6c8e
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-24 21:13:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,8 @@ def now_serving(katz_deli)
 end
 
 def take_a_number (katz_deli, name)
-  katz_deli = line
-  line.push(name)
-  puts "Welcome, #{name}. You are number #{line.length} in line."
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
